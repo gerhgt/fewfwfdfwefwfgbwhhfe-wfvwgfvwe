3b5504e1acf217814cd4ed97d5a0fde0812694e1
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-08 12:19:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1534,13 +1534,33 @@ client.on('message', message => {
         color: 0x06DF00,
         description: "يمنع النشر في هذا السيرفر",
         footer: {
-          text: "By Abo Khalil"
+          text: "By D7oM"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
 
      
 })
+
+
+
+lient.on('message',async message => {
+  if(message.content.startsWith(prefix + "svoice")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+    },1000);
+  });
+  }
+});
 
   
 client.login(process.env.BOT_TOKEN);
