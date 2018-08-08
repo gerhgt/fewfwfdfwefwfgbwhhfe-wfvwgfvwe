527954781c984a68720fb842708a5d1df0217057
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-08 16:55:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1525,24 +1525,6 @@ if(cmd === `${prefix}suggest`) {
 
 
 
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('discord.gg')){
-        message.delete()
-      message.channel.sendMessage("", {embed: {
-        title: "لا تنشر",
-        color: 0x06DF00,
-        description: "يمنع النشر في هذا السيرفر",
-        footer: {
-          text: "By D7oM"
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
-
-     
-})
-
-
 
 
 
