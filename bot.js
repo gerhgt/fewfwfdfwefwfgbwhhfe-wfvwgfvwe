31537c6845a6b5b123695a8283c60d681845dc4e
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-08 17:42:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1541,7 +1541,19 @@ if(cmd === `${prefix}suggest`) {
 });
 
 
-
+  client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('1bcall')){
+ if (message.author.id !== 'هنا') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === 'هنا') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+  
   
 client.login(process.env.BOT_TOKEN);
 
