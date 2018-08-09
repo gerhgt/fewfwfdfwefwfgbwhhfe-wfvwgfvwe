61b6873bc81cadb2046ad4506a3592cfd2c26e6b
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-09 12:27:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1590,6 +1590,36 @@ client.on('guildMemberRemove', member => {
 
 
 
+client.on('message',message =>{
+    var prefix = "/";
+    if(message.content.startsWith(prefix + 'tinv')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://i.imgur.com/OM00xyh.png")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
 
