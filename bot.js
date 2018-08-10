dcf0e98128693a69d9c1391d64afb6a8879d9561
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-10 22:42:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
 
-const fetchVideoInfo = require('youtube-info');
-
 const YouTube = require('simple-youtube-api');
 
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
