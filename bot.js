21d2f1e950d380e5a3c385cf8c86a7898bbef53d
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-11 19:14:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2551,6 +2551,25 @@ if(cmd === `${prefix}coins`) {
 
 
 
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "1roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
 
 
