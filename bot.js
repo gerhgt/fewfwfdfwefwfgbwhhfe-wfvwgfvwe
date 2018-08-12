0491aee87948723aff56da4b0fb9118182139a83
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-12 12:38:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2555,6 +2555,29 @@ if(cmd === `${prefix}coins`) {
 }
 });
 
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To The Bot")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setColor("RANDOM")
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                    .setFooter(`DM Bot Messages | DM Logs`)
+                client.users.get("299302204437823488").send(yumz)
+            }
+});
+
+
+
+
+
+
+
+
+
 
 
 
