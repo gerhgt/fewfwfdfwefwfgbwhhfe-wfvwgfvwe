91a82072945580b3a25a8f80ed2a48f726dc46ed
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-12 12:46:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1765,6 +1765,8 @@ client.on('message', message => {
 1suggest
 :large_blue_circle:   (لازم تسوي روم اسمه reports)للابلغ عن شخص
 1report
+:large_blue_circle:   لمسح الشات مع رقم
+1clr
 :large_blue_circle:   لعرض الكوينز
 1coins
 :large_blue_circle:   لاظهار شعار البوت
@@ -2578,7 +2580,33 @@ client.on("message", (message) => {
 
 
 
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "clr")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You need MANAGE_MESSAGES permission noob');
+  if (!args[0]) return message.channel.send('You didn\'t provide any number!!!');
 
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(`Cleared ${args[0]} messages.`);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'logs');
+
+    if (!actionlog) return message.channel.send('Can\'t find action-log channel. Are you sure that this channel exists and I have permission to view it? **CANNOT POST LOG.**');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('~Purge~')
+      .setColor(0xF16104)
+      .addField('Purged By', `<@${message.author.id}> with ID ${message.author.id}`)
+      .addField('Purged in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+   
+  });
+};
+
+});
 
 
 
