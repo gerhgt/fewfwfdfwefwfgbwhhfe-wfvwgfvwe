742c4a9f713822595a0b43476d9766bdae536579
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-15 11:48:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,64 +33,6 @@ msg.channel.send(pingsembed)
 
 
 
-
-Save New Duplicate & Edit Just Text Twitter
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
 const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
 const coolDown = new Set();
 
