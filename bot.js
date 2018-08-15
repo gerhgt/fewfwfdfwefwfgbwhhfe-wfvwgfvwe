5927852b1980e78c35c04ca0940f5c5da46dacdb
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-15 11:11:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2838,6 +2838,21 @@ client.on('message', async message =>{
 
 
 
+var KinG66S = {};
+client.on('guildMemberRemove', member => {
+KinG66S[member.id] = {roles: member.roles.array()};
+});
+
+client.on('guildMemberAdd', member => {
+if(!KinG66S[member.user.id]) return;
+console.log(KinG66S[member.user.id].roles.length);
+for(let i = 0; i < KinG66S[member.user.id].roles.length + 1; i++) {
+member.addRole(KinG66S[member.user.id].roles.shift());
+}
+})
+
+
+
 
 client.on('message', message => {
     if (message.author.bot) return;
