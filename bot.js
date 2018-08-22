3be284c9ab736ffada01504f1325dbef3e0bf465
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-22 11:09:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2908,8 +2908,7 @@ AdminCommands    اوامر الادارة
 ╔[❖=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=❖]╗
   ❖1say:تخلي البوت يتكلم يللي تبيه
 ╔[❖=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=❖]╗
-  ❖1set:حالة البوت
-╔[❖=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=❖]╗
+
 رابط سيرفر البوت
 https://discord.gg/m6t4Vxk
 ----------
@@ -3444,6 +3443,30 @@ client.on('message', message => {
       message.channel.send(kayan);
     
     });
+
+
+
+client.on("message", msg => {
+  if(msg.content === '#' + "id") {
+      const embed = new Discord.RichEmbed();
+  embed.addField("🔱| اسم الحساب :", `${msg.author.username}#${msg.author.discriminator}`, true)
+          .addField("🆔| الاي دي :", `${msg.author.id}`, true)
+          .setColor("RANDOM")
+          .setFooter(msg.author.username , msg.author.avatarURL)
+          .setThumbnail(`${msg.author.avatarURL}`)
+          .setTimestamp()
+          .setURL(`${msg.author.avatarURL}`)
+          .addField('📛| الحالة :', `${msg.author.presence.status.toUpperCase()}`, true)
+          .addField('🎲| بلاينج :', `${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name}`, true)
+          .addField('🏅| الرتب : ', `${msg.member.roles.filter(r => r.name).size}`, true)
+          .addField('📅| تم الانضمام للديسكورد في :', `${msg.createdAt}`,true)
+          .addField('🤖| هل هو بوت ؟', `${msg.author.bot.toString().toUpperCase()}`, true);
+      msg.channel.send({embed: embed})
+  }
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
 
