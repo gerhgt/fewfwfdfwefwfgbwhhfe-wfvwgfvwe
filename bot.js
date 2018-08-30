a332747a04179d6430fbad8c5eab98b85329bc49
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-08-30 09:16:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4031,7 +4031,7 @@ client.on('message', message => {
 
 
 
-hero.on('guildMemberAdd',async member => {
+client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');
   const jimp = require('jimp');
   const w = ['./welcome_4.png'];
