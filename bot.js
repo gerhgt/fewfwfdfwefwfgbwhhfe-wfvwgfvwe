45d757398239033d10b6bee22a193ddf925fca69
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-05 12:44:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4104,6 +4104,30 @@ member.addRole(KinG66S[member.user.id].roles.shift());//! KinG66S.❤#0045
 
 
 
+
+
+  client.on('message', message => {
+if(message.content.startsWith("1slots")) {
+  let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+  let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let we;
+  if(slots1 === slots2 && slots2 === slots3) {
+    we = "Win!"
+  } else {
+    we = "Lose!"
+  }
+  message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});
+  
+
+
+
+
+
+
   client.on('message', async ReBeLL => {
 if(ReBeLL.author.bot) return;
 if (ReBeLL.channel.guild) {
