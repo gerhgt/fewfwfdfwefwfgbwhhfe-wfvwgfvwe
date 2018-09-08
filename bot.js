cc0ef318d79c48194dfd985347edd635cad23074
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-08 21:42:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4290,6 +4290,43 @@ client.on('message',async message => {
   }
 });
 
+lient.on('message', message => {
+let perm = message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.guild.member(message.author).hasPermission('BAN_MEMBERS')
+if (!perm) return message.reply(':x: | **You don\'t have `BAN_MEMBERS` permission to use this command**.')
+if(message.content.startsWith(prefix + 'unhackban')) {
+  let nourid = message.content.split(" ").slice(3).join(" ");
+  let nour = bot.fetchUser(nourid)
+  .then(user => {
+    message.guild.unban(user.id)
+    .then(() => {
+      message.channel.send(`Alright, I unhackbanned ${user}.`)
+    }).catch(err => {
+        message.channel.send(`Failed to unban :( ${user}`)
+    })
+  }).catch(() => message.channel.send("Theres no user with the this ID :face_palm:"))
+}
+  })
+
+
+
+
+
+client.on('message', message => {
+let perm = message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.guild.member(message.author).hasPermission('BAN_MEMBERS')
+if (!perm) return message.reply(':x: | **You don\'t have `BAN_MEMBERS` permission to use this command**.')
+if(message.content.startsWith(prefix + 'hackban')) {
+  let nourid = message.content.split(" ").slice(3).join(" ");
+  client.fetchUser(nourid).then(id => {
+    message.guild.ban(id).catch(err => {
+      message.channel.send("Error 404, failed to ban this user :( -> " +id)
+      console.log(err)
+    })
+    message.channel.send(`I banned the user ${id} successfully.`)
+  }).catch(() => {
+    message.channel.send(`Theres no user with the ID of ${nourid}, please try again. :face_palm:`)
+  })
+  }});
+
 
 
 
