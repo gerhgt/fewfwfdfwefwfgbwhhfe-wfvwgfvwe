6d33ed823f9de4142e1a80f5901f3893315a0164
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-14 10:49:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,21 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
+client.oN("message", message => {
+        if (message.content === "setprefix") {
+        if (message.author.id !== "299302204437823488" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
+  let args = message.content.split(" ").slice(1);
+        let arg = args.join("").substring(message.length)
+        if (!arg) return message.channel.send(`Please add a prefix after command like \`\`${prefix}setprefix &\`\``);
+        fs.database().ref('servers/' + message.guild.id).update({
+            guildname: message.guild.name,
+            guildprefix: arg
+        }).catch(function(err) {
+            message.channel.send(err + "\n\n\n");
+        });
+        message.channel.send(`prefix updated ${arg} for ${message.guild.name}`);
+    }
+});
 client.on('message', message => {
     if (message.content == "امثال") {
         var x = ["أذا ذل رويال فهو ...",
