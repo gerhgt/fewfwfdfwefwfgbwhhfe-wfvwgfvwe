29e8c53e831da60cb38d51cdae355ea3d7c83dac
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-14 13:55:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,17 @@ message.channel.startTyping();
 
 
 
-
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('1sbc')){
+ if (message.author.id !== '299302204437823488') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
