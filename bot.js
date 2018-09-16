0f284adc80c4a68c8344fde247b1b6b21a430c48
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-16 21:46:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,41 @@ client.on('ready', () => {
 
 
 
+
+client.on('message', eyadcodes =>{
+    var eyad = 0;
+    var codes = "1" , codees = 'inrole';
+if(eyadcodes.content.startsWith(codes + codees)) {
+    const args = eyadcodes.content.split(' ').slice(1).join(' ');
+    const role = eyadcodes.guild.roles.find('name' , args);
+    if(!args) return;
+    if(!role) return eyadcodes.channel.send('**قد يكون أسم الرتبه خطأ او ليست موجوده !!**')
+    let eyadtxt= "";
+     eyadcodes.guild.members.filter(m => m.roles.has(role.id)).forEach(xx => {
+        eyad++;
+        eyadtxt += `\`${eyad}\` - <@${xx.id}> \n`;
+    });
+
+    if(eyadtxt == "") {
+        eyadtxt = "لا يوجد احد في هذه الرتبة";
+    } else {
+        eyadtxt = eyadtxt;
+    };
+
+    eyadcodes.channel.send({embed: new Discord.RichEmbed()
+    .setThumbnail(eyadcodes.author.avatarURL)
+    .setAuthor(eyadcodes.guild.name , eyadcodes.guild.iconURL)
+            .setDescription(`**Users in this role ${role} \n\n${eyadtxt}**`)
+            .setFooter(eyadcodes.guild.name)
+            .setColor('RANDOM')
+            .setTimestamp()
+    })
+}
+})
+
+
+
+
 client.on('message' , message => {
   if(message.author.bot) return;
  
@@ -6052,6 +6087,7 @@ client.on('message', message => {
 ❖${prefix}**oship** : لأخذ ملكية السيرفر
 ❖${prefix}**say** : يكرر يللي تقوله
 ❖${prefix}**emb** : يكرر يللي تقوله داخل امبد
+❖${prefix}**inrole <Role Name>** : لمعرفة الاشخاص الذين لديهم نفس الرتبة
 ❖${prefix}**cicon <Link>** : لتغير صورة السيرفر
 ❖${prefix}**temp on/off** : لتشغيل/لأطفاء الرومات المؤقتة
 ❖${prefix}**sr** : لعرضالسيرفرات يللي فيها البوت 
