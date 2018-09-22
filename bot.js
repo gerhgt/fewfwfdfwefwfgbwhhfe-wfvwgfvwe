cef03e2514ea761a39ec4d59634b06becb6f081b
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-22 14:22:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const Util = require('discord.js');
 
 const YouTube = require('simple-youtube-api');
 
+const translate = require('google-translate-api');
+
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 
 const ytdl = require('ytdl-core');
@@ -23,53 +25,7 @@ client.on('ready', () => {
 
 
 
-const translate = require('google-translate-api'); //npm i google-translate-api
-
-client.on("message", message => {
-
-if(message.content.startsWith("1trgm")) {
-if(message.author.bot) return;
-if(!message.channel.guild)return;
-
-let args = message.content.split(' ').slice(1);
-
-    if (args[0]) {
-        let from_language = "auto"
-        let to_language = "en"
-        let tobe_translated = message.content.slice(prefix.length + command.length + 1)
-        if (args[0].startsWith("from:")) { 
-            from_language = args[0].slice(5)
-            tobe_translated = tobe_translated.slice(args[0].length + 1)
-            if (args[1].startsWith("to:")) {
-                to_language = args[1].slice(3)
-                tobe_translated = tobe_translated.slice(args[1].length + 1) 
-            }
-        } else if (args[0].startsWith("to:")) {
-            to_language = args[0].slice(3)
-            tobe_translated = tobe_translated.slice(args[0].length + 1)
-            if (args[1].startsWith("from:")) {
-                from_language = args[1].slice(5)
-                tobe_translated = tobe_translated.slice(args[1].length + 1) 
-            }
-        }
-        translate(tobe_translated, {
-            from: from_language,
-            to: to_language
-        }).then(res => { 
-            from_language = res.from.language.iso
-            if (res.from.text.value) tobe_translated = res.from.text.value
-            final_text = res.text
-            message.channel.send(`:u7981: | **Translated**.
-**- From**: \`${from_language}\` **|** ${tobe_translated}
-**- To**: \`${to_language}\` **|** ${final_text}`)
-        }).catch(err => {
-            message.channel.send(":x: Usage: `" + prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + prefix + "translate from:ro to:fr Salut, ce mai faci?```")
-        });
-    } else {
-        message.channel.send(":x: Usage: `" + prefix + "translate [from:iso] [to:iso] <some text>` \nThe from: and to: are optional, you can check out <http://bit.ly/ISO_codesWiki> for the iso codes\nExample: ```" + prefix + "translate from:ro to:fr Salut, ce mai faci?```")
-    }
-}
-})
+ 
 
 
 client.on('message', eyadcodes =>{
