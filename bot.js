2b3fc3dce96f4a001fa3b30a44bbf172a4cb3cf6
gerhgt <42190114+gerhgt@users.noreply.github.com>
2018-09-23 16:16:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,23 @@ client.on('ready', () => {
 
 
 
+client.on('message', message => {
+if(message.content.startsWith("1slots")) {
+  let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+  let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let we;
+  if(slots1 === slots2 && slots2 === slots3) {
+    we = "Win!"
+  } else {
+    we = "Lose!"
+  }
+  message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});
+
+
    
 client.on("message", (message) => {
     /// ALPHA CODES
